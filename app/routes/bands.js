273b2273b0fe2ed88ebf1c95e3d9a9b7ad8fb71a
Anne-G <ang.schall@gmail.com>
2021-03-17 11:48:51 +0100

    Seance 3: add model to bands

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class BandsRoute extends Route {
+  model() {
+    return [
+      { name: 'Led Zeppelin' },
+      { name: 'Pearl Jam' },
+      { name: 'Foo Fighters' },
+    ];
+  }
 }
