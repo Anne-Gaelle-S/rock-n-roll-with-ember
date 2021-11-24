4c09206803e41c58c7226c1e904a65bf8dda3305
Anne-G <ang.schall@gmail.com>
2021-11-24 11:16:52 +0100

    lint all files

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service'; import Song from 'rarwe/models/song';
+import { inject as service } from '@ember/service';
+import Song from 'rarwe/models/song';
 import fetch from 'fetch';
 
 export default class BandsBandSongsRoute extends Route {
